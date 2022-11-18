3fb9a6decd6b42fc08ee6c49cdafc60a4ad299b8
amine <aminealm7@gmail.com>
2022-11-18 19:03:12 +0100

    'test'

--- src/components/Career/Career.js
+++ src/components/Career/Career.js
@@ -5,7 +5,7 @@ import DataInfo from './DataInfo';
 import Pagination  from '../Pagination/Pagination';
 
 function Career() {
-    const url='https://jsonplaceholder.typicode.com/posts'
+    const url='https://mockend.com/org/repo/posts'
 
     const [data,setData] = useState([]);
 
